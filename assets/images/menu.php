995b618849b304b87e6a3728705c3eddf21b417d
Al Muhdil Karim <al.muhdil.0@gmail.com>
2022-06-29 16:43:29 +0700

    beta.0.1

---
+++ assets/images/menu.php
@@ -0,0 +1,11 @@
+<div class="logo">
+    <img src="<?php echo get_template_directory_uri();?>/assets/images/logo-uin.png" width="80" height="80">
+</div>
+<nav>
+    <ul>
+        <li><a href="#1"><span class='fa fa-desktop'></span> <em>About</em></a></li>
+        <li><a href="#2"><span class='fa fa-desktop'></span> <em>Class</em></a></li>
+        <li><a href="#3"><span class='fa fa-desktop'></span> <em>Office</em></a></li>
+        <li><a href="#4"><span class='fa fa-desktop'></span> <em>Support</em></a></li>
+    </ul>
+</nav>
